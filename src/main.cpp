bcf2d166f6c1fe8a220c30271c66cf9ba183193e
zawkindev <zawkindev@gmail.com>
2024-04-20 18:54:41 +0500

    fix: printMenu function

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
-// #include "../include/globals.h"
+#include "../include/globals.h"
 #include "../include/task_manager.h"
 
 int main() {
-  string test_menu[] = {"Main menu", "afa", "edit"};
-  printNavbar(test_menu, arraySize(test_menu));
 
   return 0;
 }
